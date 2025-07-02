4db3682d1fb686ca21343ddcf657eca8213f6106
Alexandru Gheoala <162614973+Agheoala19@users.noreply.github.com>
2025-07-02 14:58:30 +0300

    Create Task.java

---
+++ src/model/Task.java
@@ -0,0 +1,46 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Task {
+	private int id;
+	private String description;
+	private boolean completed;
+	private LocalDate deadline;
+	
+	public Task(int id, String description, LocalDate deadline) {
+		this.id = id;
+		this.description = description;
+		this.deadline = deadline;
+		this.completed = false;
+	}
+	
+	public void markCompleted() {
+		this.completed = true;
+	}
+	
+	public boolean isCompleted() {
+		return this.completed;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+  public String getDescription() {
+      return description;
+  }
+
+  public LocalDate getDeadline() {
+      return deadline;
+  }
+
+  @Override
+  public String toString() {
+      String status = completed ? "✓" : "✗";
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+      return String.format("[%d] %s - %s (Deadline: %s)", 
+                          id, status, description, deadline.format(formatter));
+  }
+}
