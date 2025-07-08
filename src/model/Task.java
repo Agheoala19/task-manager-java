43f4fbabb751b97f441f3b271f5e0fd5b1f33d80
Alexandru Gheoala <162614973+Agheoala19@users.noreply.github.com>
2025-07-08 15:50:48 +0300

    Update Task.java

--- src/model/Task.java
+++ src/model/Task.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import java.time.format.DateTimeFormatter;
 
 public class Task implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
 	private int id;
 	private String description;
 	private boolean completed;
